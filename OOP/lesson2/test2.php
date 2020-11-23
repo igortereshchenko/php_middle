0fe57dcdb5b907020ab1fd6b23b58ceca0ef0257
igortereshchenko <tereshchenko.igor@gmail.com>
2020-11-23 14:21:38 +0200

    oop lesson 2 task

---
+++ OOP/lesson2/test2.php
@@ -0,0 +1,39 @@
+<?php
+//TODO
+"
+Vehicle  <-Parent
+fields
+    passanger_count
+    speed
+    color
+construct
+methods
+ toString
+
+
+
+Car <-Child 1
+
+fields
+    engine_type
+    model
+construct
+methods
+ toString
+    
+    
+Tram <-Child 2
+fileds
+    carriage_count
+    rail_type
+construct    
+methods
+ toString
+ profit (ticket_price) => return passanger_count*ticket_price
+    
+
+
+
+
+
+";
